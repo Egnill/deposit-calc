9a88f9140175af5561593e40b2395454c44281b0
Egnill <alex_30199830@mail.ru>
2017-02-15 09:09:06 +0700

    Changed function

--- dc.c
+++ dc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void prover(float vklad, int day);
+void prover(float vklad, int day, float *rez);
 int main()
 {
 	int day;
@@ -11,7 +11,7 @@ int main()
 	
 	prover(vklad, day, &rez);
 	
-	printf("Размер вклада на конец срока(в т. р.): %2f", rez);
+	printf("Размер вклада на конец срока(в т. р.): %.2f\n", rez);
 	
 	return 0;
 }
@@ -20,30 +20,30 @@ void prover(float vklad, int day, float *rez)
 {
 	if (vklad <= 100) {
 		if (day <= 0 && day <= 30) {
-				rez = vklad * 0.9;
+				*rez = vklad * 0.9;
 			}
 		if (day <= 31 && day <= 120) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 		if (day <= 121 && day <= 240) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 		if (day <= 241 && day <= 365) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 	} else {
 		if (vklad >= 100) {
 			if (day <= 0 && day <= 30) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 			if (day <= 31 && day <= 120) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 			if (day <= 121 && day <= 240) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 			if (day <= 241 && day <= 365) {
-				rez = vklad * 1.03;
+				*rez = vklad * 1.03;
 			}
 		}
 	}
