fabed6d5b79dcbf456d5b15ffc31225857722919
Egnill <alex_30199830@mail.ru>
2017-02-15 09:16:59 +0700

    Changed function else

--- dc.c
+++ dc.c
@@ -4,7 +4,7 @@ void prover(float vklad, int day, float *rez);
 int main()
 {
 	int day;
-	float vklad, rez;
+	float vklad, rez = 0;
 	
 	printf("Введите сумму вклада(в т.р) и срок вклада: ");
 	scanf("%f%d", &vklad, &day);
@@ -19,31 +19,31 @@ int main()
 void prover(float vklad, int day, float *rez)
 {
 	if (vklad <= 100) {
-		if (day <= 0 && day <= 30) {
+		if (day >= 0 && day <= 30) {
 				*rez = vklad * 0.9;
 			}
-		if (day <= 31 && day <= 120) {
-				*rez = vklad * 1.03;
+		if (day >= 31 && day <= 120) {
+				*rez = vklad * 1.02;
 			}
-		if (day <= 121 && day <= 240) {
-				*rez = vklad * 1.03;
+		if (day >= 121 && day <= 240) {
+				*rez = vklad * 1.06;
 			}
-		if (day <= 241 && day <= 365) {
-				*rez = vklad * 1.03;
+		if (day >= 241 && day <= 365) {
+				*rez = vklad * 1.12;
 			}
 	} else {
-		if (vklad >= 100) {
-			if (day <= 0 && day <= 30) {
+		if (vklad > 100) {
+			if (day >= 0 && day <= 30) {
+				*rez = vklad * 0.9;
+			}
+			if (day >= 31 && day <= 120) {
 				*rez = vklad * 1.03;
 			}
-			if (day <= 31 && day <= 120) {
-				*rez = vklad * 1.03;
+			if (day >= 121 && day <= 240) {
+				*rez = vklad * 1.08;
 			}
-			if (day <= 121 && day <= 240) {
-				*rez = vklad * 1.03;
-			}
-			if (day <= 241 && day <= 365) {
-				*rez = vklad * 1.03;
+			if (day >= 241 && day <= 365) {
+				*rez = vklad * 1.15;
 			}
 		}
 	}
